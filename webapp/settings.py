c0b1944b2bb289ec72eb2eecfa9e188e51cf4413
bgla29 <bryan.german.lema@gmail.com>
2024-11-20 21:19:34 -0500

    Remove password

--- webapp/settings.py
+++ webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%j2$y%a-38e^yn1@b4t!5246=sra$+_0wdwu5i)dxt+=955&i%'
+SECRET_KEY = '*************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
